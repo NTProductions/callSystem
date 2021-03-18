a24810f15b7ec6e841580609e61cd226ca19ad27
Nate Lovell <Natelovell99@gmail.com>
2021-03-18 04:09:13 -0600

    Create callSystem.jsx

---
+++ callSystem.jsx
@@ -0,0 +1,3 @@
+﻿var timeStr = system.callSystem("cmd.exe /c \"time /t\"");
+// the call system command will return whatever the command line would normally output from your input
+alert("Current time is " + timeStr);
